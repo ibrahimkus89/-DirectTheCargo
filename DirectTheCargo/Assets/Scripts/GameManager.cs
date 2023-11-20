1a8d1baa406a3e9ee98730ea451006fc672cc694
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2023-11-20 16:02:21 +0300

    all panels

--- DirectTheCargo/Assets/Scripts/GameManager.cs
+++ DirectTheCargo/Assets/Scripts/GameManager.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -17,15 +22,21 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Transform _BoxExitPoint;
     private int _BoxPoolIndex;
     public float _CheckOutTime;
-
     int _C_BoxNumber;
+    private int _sceneIndex;
 
     [Header("-----SOUND MANAGEMENT")]
     [SerializeField] AudioSource[] _AudioSources;
+    [SerializeField] private Image[] _SoundSettingsButtons;
+    [SerializeField] private Sprite[] _SpriteObj;
 
-
+        [Header("-----SOUND MANAGEMENT")]
+    [SerializeField] private GameObject[] _Panels;
+    [SerializeField] private TextMeshProUGUI[] _ScoreTexts;
     void Awake()
     {
+        _sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
         if (Instance ==null)
         {
             Instance = this;
@@ -38,8 +49,6 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(SendBox());
-
         _activeLine = _DefaultExitLines;
          SceneProcess();
     }
@@ -77,21 +86,25 @@ public class GameManager : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                if (Physics.Raycast(_Ray,out RaycastHit hit,100))
+                if (!UII())
                 {
-                    if (hit.transform.gameObject.CompareTag("Line"))
+                    if (Physics.Raycast(_Ray, out RaycastHit hit, 100))
                     {
-                        if (_activeLine !=null)
+                        if (hit.transform.gameObject.CompareTag("Line"))
                         {
-                           _activeLine.LightProcess(0);
+                            if (_activeLine != null)
+                            {
+                                _activeLine.LightProcess(0);
+                            }
+
+                            _activeLine = hit.transform.gameObject.GetComponent<ExitLine>();
+                            _activeLine.LightProcess(1);
+                            //Debug.Log("City Name : " +_activeLine._CityName);
+
                         }
-
-                       _activeLine = hit.transform.gameObject.GetComponent<ExitLine>();
-                        _activeLine.LightProcess(1);
-                        //Debug.Log("City Name : " +_activeLine._CityName);
-
                     }
                 }
+               
             }
         }
 
@@ -125,8 +138,9 @@ public class GameManager : MonoBehaviour
     {
         PlaySound(2);
         _C_BoxNumber++;
+        _ScoreTexts[0].text = _C_BoxNumber.ToString();
         GameSpeed(_C_BoxNumber);
-        Debug.Log("BoxNumber" +_C_BoxNumber);
+      
 
         /* if (_C_BoxNumber ==5)
          {
@@ -205,21 +219,165 @@ public class GameManager : MonoBehaviour
             PlayerPrefs.SetInt("HighScore", _C_BoxNumber);
             Debug.Log("NEW SCORE : " + _C_BoxNumber);
         }
-
+        _ScoreTexts[2].text = _C_BoxNumber.ToString();
+        _ScoreTexts[3].text = PlayerPrefs.GetInt("HighScore").ToString();
+        PanelProcess(0, true);
         Time.timeScale = 0;
 
     }
 
     void SceneProcess()
     {
+        if (PlayerPrefs.GetInt("GameSound") == 1)
+        {
+            
+            _SoundSettingsButtons[0].sprite = _SpriteObj[0];
+            _AudioSources[0].mute = false;
+        }
+        else
+        {
+            
+            _SoundSettingsButtons[0].sprite = _SpriteObj[1];
+            _AudioSources[0].mute = true;
+        }
+
+        if (PlayerPrefs.GetInt("EffectSound") == 1)
+        {
+           
+            _SoundSettingsButtons[1].sprite = _SpriteObj[2];
+
+            for (int i = 1; i < _AudioSources.Length; i++)
+            {
+                _AudioSources[i].mute = false;
+            }
+        }
+        else
+        {
+            
+            _SoundSettingsButtons[1].sprite = _SpriteObj[3];
+
+            for (int i = 1; i < _AudioSources.Length; i++)
+            {
+                _AudioSources[i].mute = true;
+            }
+        }
+
         if (!PlayerPrefs.HasKey("HighScore"))
         {
             PlayerPrefs.SetInt("HighScore", 0);
+            PlayerPrefs.SetInt("GameSound", 1);
+            PlayerPrefs.SetInt("EffectSound", 1);
+
         }
+
+        _ScoreTexts[1].text = PlayerPrefs.GetInt("HighScore").ToString();
     }
 
     void PlaySound(int Indexx)
     {
         _AudioSources[Indexx].Play();
+    }
+
+    void PanelProcess(int Index,bool situation)
+    {
+        _Panels[Index].SetActive(situation);
+    }
+
+    public void ButtonProcess(string Valuee)
+    {
+        switch (Valuee)
+        {
+            case "Start":
+                PlaySound(1);
+                PanelProcess(0,false);
+                PanelProcess(4, true);
+                StartCoroutine(SendBox());
+                break;
+
+            case "Pause":
+                PlaySound(1);
+                PanelProcess(1, true);
+                Time.timeScale = 0;
+                break;
+
+            case "Continue":
+                PlaySound(1);
+                PanelProcess(1, false);
+                Time.timeScale = 1;
+                break;
+
+            case "TryAgain":
+                PlaySound(1);
+                SceneManager.LoadScene(_sceneIndex);
+                Time.timeScale = 1;
+                break;
+
+            case "Exit":
+                PlaySound(1);
+                PanelProcess(3, true);
+                break;
+
+            case "Yes":
+                PlaySound(1);
+                Debug.Log("Quit");
+                Application.Quit();
+                break;
+            case "No":
+                PlaySound(1);
+                PanelProcess(3, false);
+                break;
+            case "GameSoundSettings":
+                PlaySound(1); 
+                
+                if (PlayerPrefs.GetInt("GameSound") ==0)
+                {
+                    PlayerPrefs.SetInt("GameSound",1);
+                    _SoundSettingsButtons[0].sprite = _SpriteObj[0];
+                    _AudioSources[0].mute =false;
+                }
+                else
+                {
+                    PlayerPrefs.SetInt("GameSound",0);
+                    _SoundSettingsButtons[0].sprite = _SpriteObj[1];
+                    _AudioSources[0].mute = true;
+                }
+                break;
+            case "EffectSoundSettings":
+                PlaySound(1);
+
+                if (PlayerPrefs.GetInt("EffectSound") == 0)
+                {
+                    PlayerPrefs.SetInt("EffectSound", 1);
+                    _SoundSettingsButtons[1].sprite = _SpriteObj[2];
+
+                    for (int i = 1 ;i < _AudioSources.Length; i++)
+                    {
+                        _AudioSources[i].mute = false;
+                    }
+                }
+                else
+                {
+                    PlayerPrefs.SetInt("EffectSound", 0);
+                    _SoundSettingsButtons[1].sprite = _SpriteObj[3];
+
+                    for (int i = 1; i < _AudioSources.Length; i++)
+                    {
+                        _AudioSources[i].mute = true;
+                    }
+                }
+                break;
+        }
+    }
+
+    bool UII()
+    {
+        if (Input.touchCount >0 && Input.touches[0].phase ==TouchPhase.Began)
+        {
+            if (EventSystem.current.IsPointerOverGameObject(Input.touches[0].fingerId))
+            {
+                return true;
+            }
+        }
+        return false; 
     }
 }
